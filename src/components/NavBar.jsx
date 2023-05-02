aca1900e6bcd452b648a234713fbdea0f530493f
soloman-err <soloman.err@gmail.com>
2023-05-02 06:56:01 +0600

    nav links active state implemented

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+  const [activeState, setActiveState] = useState("");
+  const location = useLocation();
+  console.log(activeState);
+
+  useEffect(() => {
+    setActiveState(location.pathname);
+  }, [location.pathname]);
+
+  const activeStyle = {
+    fontWeight: "bold",
+  };
+
   return (
     <nav className="flex items-center justify-between">
       <h1 className="text-2xl font-bold">Cocinero</h1>
 
       <ul className="flex gap-5">
-        <Link to="/">Home</Link>
-        <Link to="blog">Blog</Link>
-        <Link to="about">About us</Link>
+        <Link to="/" style={activeState == "/" ? activeStyle : null}>
+          Home
+        </Link>
+        <Link to="blog" style={activeState == "/blog" ? activeStyle : null}>
+          Blog
+        </Link>
+        <Link to="about" style={activeState == "/about" ? activeStyle : null}>
+          About us
+        </Link>
       </ul>
 
       <div>
