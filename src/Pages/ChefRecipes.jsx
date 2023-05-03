90d1adee77d8785a8f46c56a7fb47a2545c941b5
soloman-err <soloman.err@gmail.com>
2023-05-03 16:02:22 +0600

    Recipe cards functionality updated

--- src/Pages/ChefRecipes.jsx
+++ src/Pages/ChefRecipes.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { FaBookmark, FaHeart, FaUser, FaWifi } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 import { useLoaderData } from "react-router-dom";
-import Swal from "sweetalert2";
+import RecipeCard from "../components/RecipeCard";
 
 const ChefRecipes = () => {
   const [recipes, setRecipes] = useState([]);
@@ -12,13 +12,6 @@ const ChefRecipes = () => {
   console.log(chefInfo);
   const { id, image, thumbnail, name, numLikes, numRecipes, bio, experience } =
     chefInfo;
-
-  // handle favorite:
-  const handleFavorite = (id) => {
-    // const favorite = [];
-    console.log(id);
-    Swal.fire("Added to Favorite!", "check more dishes", "success");
-  };
 
   useEffect(() => {
     try {
@@ -54,10 +47,6 @@ const ChefRecipes = () => {
             </small>
           </div>
           <p className="text-slate-400">{bio}</p>
-
-          {/* <div className="card-actions justify-end">
-          <button className="btn btn-primary">Listen</button>
-          </div> */}
         </div>
         <div className="mt-2">
           <h2 className="text-slate-300 text-xl font-bold underline underline-offset-4">
@@ -68,49 +57,7 @@ const ChefRecipes = () => {
         {/* Recipes Card Group: */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 rounded my-5 shadow-2xl">
           {recipes.map((recipe) => (
-            <div
-              key={recipe.id}
-              className="card bg-base-100 shadow-xl border border-slate-700 overflow-hidden"
-            >
-              <img src={recipe.thumbnail} alt="" />
-
-              <div className="card-body relative">
-                <h4 className="font-bold text-2xl">
-                  {recipe.title}
-                  <FaBookmark
-                    onClick={() => handleFavorite(recipe.id)}
-                    className="absolute right-2 top-10 text-2xl text-amber-600"
-                  />
-                </h4>
-                <p className="underline underline-offset-4 text-amber-600 font-bold">
-                  Ingredients:
-                </p>
-                {/* <ul className=" text-sm text-slate-300 list-inside list-disc"> */}
-                {/* {recipe.ingredients?.map((ingredient) => {
-                    ingredient.map((i) => <li>{i}</li>);
-                  })} */}
-                {/* {console.log(recipe?.ingredients)}
-                  {recipe.ingredients.map((i) => (
-                    <li key={i}>{i}</li>
-                  ))} */}
-
-                {/* {recipe.ingredients} */}
-                {/* </ul> */}
-                {/* {recipe.ingredients[0]} */}
-                {/* <p>
-                  {console.log(
-                    recipe.ingredients.map((i) => {
-                      i;
-                    })
-                  )}
-                </p> */}
-
-                <p className="underline underline-offset-4 text-amber-600 font-bold">
-                  Cooking Method:
-                </p>
-                <p className="text-slate-300">{recipe.method}</p>
-              </div>
-            </div>
+            <RecipeCard key={recipe.id} recipe={recipe}></RecipeCard>
           ))}
         </div>
       </div>
@@ -119,41 +66,3 @@ const ChefRecipes = () => {
 };
 
 export default ChefRecipes;
-// {
-//     "id": 1,
-//     "thumbnail": "https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-//     "title": "Spaghetti Carbonara",
-//     "ingredients": [
-//         {
-//             "id": 1,
-//             "name": "Spaghetti",
-//             "image": "https://images.unsplash.com/photo-1610528521026-c36f6cf64d58?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHNwYWdldHRpfGVufDB8fDB8fA%3D%3D&ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60",
-//             "desc": "Long, thin, cylindrical pasta."
-//         },
-//         {
-//             "id": 2,
-//             "name": "Eggs",
-//             "image": "https://images.unsplash.com/photo-1580928251474-21a0a22fcb56?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZWdnc3xlbnwwfHwwfHw%3D&ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60",
-//             "desc": "Chicken eggs."
-//         },
-//         {
-//             "id": 3,
-//             "name": "Parmesan Cheese",
-//             "image": "https://images.unsplash.com/photo-1608605667644-4f4b8d258b4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8cGFybWVzYW4lMjBjaGVlc2V8ZW58MHx8MHx8&ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60",
-//             "desc": "Hard, granular cheese."
-//         },
-//         {
-//             "id": 4,
-//             "name": "Salsa",
-//             "image": "https://images.unsplash.com/photo-1596758704155-29c7af5a1801?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmFjb24lMjBpbWFnZXN8ZW58MHx8MHx8&ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60",
-//             "desc": "Cured pork belly."
-//         },
-//         {
-//             "id": 5,
-//             "name": "Black Pepper",
-//             "image": "https://images.unsplash.com/photo-1555072954-4f4f4f4f4f4f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJsYWNrJTIwcGVwcGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-4.0.3&auto=format&fit",
-//             "desc": "Spice commonly used for seasoning."
-//         }
-//     ],
-//     "method": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolorem."
-// }
---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FaBookmark } from "react-icons/fa";
+import Swal from "sweetalert2";
+
+const RecipeCard = ({ recipe }) => {
+  const { id, title, thumbnail, method, ingredients } = recipe;
+  console.log(ingredients);
+
+  // handle favorite:
+  const handleFavorite = (id) => {
+    console.log(id);
+    Swal.fire("Added to Favorite!", "check more dishes", "success");
+  };
+
+  return (
+    <div
+      key={id}
+      className="card bg-base-100 shadow-xl border border-slate-700 overflow-hidden"
+    >
+      <img src={thumbnail} alt="" />
+
+      <div className="card-body relative">
+        <h4 className="font-bold text-2xl">
+          {title}
+          <FaBookmark
+            onClick={() => handleFavorite(id)}
+            className="absolute right-2 top-10 text-2xl text-amber-600"
+          />
+        </h4>
+        <p className="underline underline-offset-4 text-amber-600 font-bold">
+          Ingredients:
+        </p>
+
+        {/* Nested map for access ingredients: */}
+        <ul className=" text-sm text-slate-300 list-inside list-disc">
+          {ingredients.map((item, index) => {
+            return <li key={index}>{item.name}</li>;
+          })}
+        </ul>
+        <p className="underline underline-offset-4 text-amber-600 font-bold">
+          Cooking Method:
+        </p>
+        <p className="text-slate-300">{method}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
